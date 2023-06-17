7863976176f2b3b763d72eaca8ef6b6e3155d2cf
raffallves <89459281+raffallves@users.noreply.github.com>
2023-06-17 17:13:09 -0300

    Arruma erro de client side navigation

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,13 @@
 import './NavBar.css'
-import { useLocation } from 'react-router-dom'
+import { useLocation, Link } from 'react-router-dom'
 
 export default function NavBar() {
     const location = useLocation()
     return (
         <nav>
             {location.pathname === '/' ?
-            <a href={'/historico'}>Histórico</a> :
-            <a href={'/'}>Validação</a>}
+            <Link to='historico'>Histórico</Link> :
+            <Link to='/'>Validação</Link>}
         </nav>
     )
 }
